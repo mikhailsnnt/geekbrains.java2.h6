c2a740729ca99701b242c2b796e54188fa85829e
Mihail Nikitin <nitinin.miha@icloud.com>
2021-11-05 14:56:02 +0300

    Completed homework

--- src/geekbrains/java6/server/ServerMain.java
+++ src/geekbrains/java6/server/ServerMain.java
@@ -1,8 +1,39 @@
 package geekbrains.java6.server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ServerMain {
 
     public static void main(String[] args) {
-	// write your code here
+        try {
+            ServerSocket serverSocket = new ServerSocket( 3071);
+            Socket client = serverSocket.accept();
+            System.out.println("Client connected");
+            new Thread(()->{
+                Scanner scanner = new Scanner(System.in);
+                try{
+                DataOutputStream out =  new DataOutputStream( client.getOutputStream());
+                while(true){
+                    out.writeUTF( scanner.nextLine());
+                }
+                }
+                catch (IOException exception){
+                    exception.printStackTrace();
+                }
+
+            }).start();
+            DataInputStream in = new DataInputStream(client.getInputStream());
+            while(true){
+                System.out.println( "Message from client: "+in.readUTF());
+            }
+        }
+        catch (IOException exception){
+            exception.printStackTrace();
+        }
     }
 }
